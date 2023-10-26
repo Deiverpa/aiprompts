82ff5e3e7156653486edf65c2fe60c4f44cd6b15
Deiverpa <deipa280@hotmail.com>
2023-10-26 04:47:55 -0500

    form start and login working

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Link from "next/link";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ type, post, setPost, submitting, handdleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        {" "}
+        <span className="blue_gradient"> {type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} & share awesome AI prompts with the world. Let your imagination
+        run free with AI-powered pla
+      </p>
+    </section>
+  );
 };
 
 export default Form;
